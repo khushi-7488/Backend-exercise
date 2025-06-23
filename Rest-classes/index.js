fdefa3a3bf0ce184cd2691fbc99c785770c06dde
khushi-7488 <khushiitiwari88@gmail.com>
2025-06-23 14:04:01 +0530

    Adding Rest API

--- Rest-classes/index.js
+++ Rest-classes/index.js
@@ -8,7 +8,22 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"))
 app.set(express.static(path.join(__dirname, "public")))
 
-app.get("/", (req, res) => {
+let posts = [
+    {
+        username: "khushi",
+        content: "I'm am a girl",
+    },
+    {
+        username: "Mayank",
+        content: "I'm am a boy",
+    },
+    {
+        username: "Rahulkumar",
+        content: "I'm am a girl",
+    }
+]
+
+app.get("/posts", (req, res) => {
     res.send("server is working");
 })
 app.listen(port, () => {
