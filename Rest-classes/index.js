0c52536ccfb0a41aff67dfe73b880578576df1a7
khushi-7488 <khushiitiwari88@gmail.com>
2025-06-23 13:50:05 +0530

    Adding Rest API

---
+++ Rest-classes/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"))
+app.set(express.static(path.join(__dirname, "public")))
+
+app.get("/", (req, res) => {
+    res.send("server is working");
+})
+app.listen(port, () => {
+    console.log("hii I'm listening");
+})
