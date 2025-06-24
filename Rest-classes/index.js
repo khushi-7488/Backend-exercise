aa908e9bc3365dbd4998e64f79518d18f95c6a26
khushi-7488 <khushiitiwari88@gmail.com>
2025-06-24 17:43:00 +0530

    Rest API

--- Rest-classes/index.js
+++ Rest-classes/index.js
@@ -2,26 +2,30 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
-app.use(express.urlencoded({ extended: true }));
+const { v4: uuidv4 } = require('uuid');
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"))
-app.use(express.static(path.join(__dirname, "public")))
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
 
 let posts = [
     {
-        id: "1a",
+        id: uuidv4(),
         username: "khushi",
         content: "I'm am a girl",
     },
     {
 
-        id: "2a",
+        id: uuidv4(),
         username: "Mayank",
         content: "I'm am a boy",
     },
     {
-        id: "3a",
+        id: uuidv4(),
         username: "Rahulkumar",
         content: "I'm am a girl",
     }
@@ -35,15 +39,23 @@ app.get("/posts/new", (req, res) => {
 })
 app.post("/posts", (req, res) => {
     let { username, content } = req.body;
-    posts.push({ username, content });
+    let id = uuidv4();
+    posts.push({ id, username, content });
     console.log(req.body);
     res.redirect("/posts");
 })
 app.get("/posts/:id", (req, res) => {
     let { id } = req.params;
+    console.log(id);
     let post = posts.find((p) => id === p.id);
-    console.log(post);
-    res.send("request sent")
+    res.render("show.ejs", { post })
+})
+app.delete("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    // console.log(id);
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect("/posts");
+
 })
 
 app.listen(port, () => {
