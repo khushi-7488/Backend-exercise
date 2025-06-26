79fdcc6958f8ffaf482872af268be0cff322adf4
khushi-7488 <khushiitiwari88@gmail.com>
2025-06-26 20:00:06 +0530

    mongoose

--- Mongo/index.js
+++ Mongo/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 main().then((res) => {
-    console.log("successfull");
+    console.log("connection successfull");
 })
     .catch(err => console.log(err));
 
@@ -9,3 +9,10 @@ async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/test');
 
 }
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: Int
+})
+
+const User = mongoose.model("User",userSchema);
